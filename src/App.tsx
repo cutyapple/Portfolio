56c31b61d9f6bedec1c0cca6a08e19fabd95c836
cutyapple <dbtldhs0915@naver.com>
2021-04-17 23:31:58 +0900

    [FEAT] <src/App.tsx>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { GlobalStyle } from './GlobalStyle';
+import Section from './components/section/Section';
+import Navigation from './components/navigation/Navigation';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Navigation />
+      <Section />
+    </>
+  );
 };
 
 export default App;
