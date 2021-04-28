9da50694d1e507e1304a2dbef146917a26d58921
CutyApple <dbtldhs0915@naver.com>
2021-04-28 15:12:35 +0900

    [FEAT] <src/components/navigation> set CSS

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './style';
+
+const Navigation = () => {
+  return <S.NavContainer></S.NavContainer>;
+};
+
+export default Navigation;
---
+++ src/components/navigation/style/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.nav`
+  width: 13rem;
+  height: 100%;
+  background: #181818;
+`;
