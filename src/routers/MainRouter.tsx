bd8fcdb5879be8a72ed6167a2401a2fd1eb4cf95
CutyApple <dbtldhs0915@naver.com>
2021-04-28 14:57:18 +0900

    [FEAT] <src/routers/MainRouter.tsx> apply MainPage

---
+++ src/routers/MainRouter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import MainPage from '../page/main/MainPage';
+
+const MainRouter = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={MainPage} />
+    </Switch>
+  );
+};
+
+export default MainRouter;
