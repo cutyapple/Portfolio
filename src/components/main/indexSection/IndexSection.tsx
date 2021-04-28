8fcd24f884b6ac3fba446f7f80f0980f8e931a0d
CutyApple <dbtldhs0915@naver.com>
2021-04-28 15:12:54 +0900

    [FEAT] <src/components/main/indexSection> set CSS

---
+++ src/components/main/indexSection/IndexSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './style';
+
+const IndexSection = () => {
+  return <S.SectionContainer></S.SectionContainer>;
+};
+
+export default IndexSection;
---
+++ src/components/main/indexSection/style/index.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const SectionContainer = styled.section`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: #1d1d1d;
+`;
